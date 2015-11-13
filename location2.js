73dd82f034c8cec8bfdd22be010f50bb1da68f8f
griffinjameskeller <kellg428@newschool.edu>
2015-11-13 15:23:11 -0500

    edits made

--- location2.js
+++ location2.js
@@ -1,21 +1,54 @@
 var city;
 
 function preload() {
-  
-  var url = 'http://api.openweathermap.org/data/2.5/weather?q=New York,NY'+
-   '&APPID=f02124924447c73bc1d1626b1bee5f45';
+
+   var url = 'http://api.openweathermap.org/data/2.5/weather?q=Miami,FL'+
+    '&APPID=f02124924447c73bc1d1626b1bee5f45&units=imperial';
   city = loadJSON(url);
+  waterdrops = loadImage("images/water.png");
+  theSun = loadImage("images/sun.png");
+  theMoon = loadImage("images/moon.png");
 
-}
 
-function setup() {
-  
+  }
+
+  function setup() {
+
   createCanvas(400,400);
+  temp = city.main.temp;
+  humid = city.main.humidity;
+  sunset = new Date(city.sys.sunset*1000);
+  sunrise = new Date(city.sys.sunrise*1000);
+  time = Date.now();
+  sea = (city.main.sea_level - 800);
+
+  }
+
+  function draw() {
 
 
-}
-
-function draw() {
+  background(0,0,temp);
 
 
-}
+      if (time > sunrise) {
+              if (time < sunset){
+                image(theSun, 30, 30, 100, 100, 0, 0, 100, 100);
+              } else {
+                image(theMoon, 30, 30, 100, 100, 0, 0, 100, 100);
+              }
+
+          } else{
+          image(theMoon, 30, 30, 100, 100, 0, 0, 100, 100);
+          }
+
+  noStroke();
+  fill('rgba(0,50,255, 0.10)');
+  rect(0, sea, 500, 500);
+
+  tint(255, humid);
+  image(waterdrops, 0, 0);
+
+
+
+
+  }
